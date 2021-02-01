7a00163c8160ac85edf88203f98bcc7f2670737a
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-01 00:55:13 -0500

    created place holders in the results and saved cards for where the data will populate

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -7,8 +7,19 @@ function Results() {
             <div className="card-body">
                 <h5>Results</h5>
                 <div id="result" className="card">
-                    <div className="card-body">
-                        mapped results will go here
+                    <div id="resultTitleAuthorButtons" className="card-body">
+                        <div>
+                            <p>book title</p>
+                            <p>author</p>
+                        </div>
+                        <div id="btnDivResults">
+                            <button id="resultView" className="resultButtons">View</button>
+                            <button id="resultSave" className="resultButtons">Save</button>
+                        </div>
+                    </div>
+                    <div id="resultsImgDescription">
+                        <p>image here</p>
+                        <p id="resultsDescription">description here</p>
                     </div>
                 </div>
             </div>
--- client/src/components/SavedCard/SavedCard.js
+++ client/src/components/SavedCard/SavedCard.js
@@ -7,8 +7,21 @@ function SavedCard() {
             <div className="card-body">
                 <h5>Saved Books</h5>
                 <div id="saved" className="card">
-                    <div className="card-body">
-                        saved books will go here
+                    <div id="savedTitleAuthorButtons" className="card-body">
+                        <div>
+                            <p>book title</p>
+                            <p>author</p>
+                        </div>
+                        <div id="btnDiv">
+                            <button id="view" className="savedButtons">View</button>
+                            <button id="delete" className="savedButtons">Delete</button>
+                        </div>
+                    </div>
+                    <div id="savedImgDescription">
+                        <p>image here</p>
+                        <p id="savedDescription">
+                            description here
+                        </p>
                     </div>
                 </div>
             </div>
